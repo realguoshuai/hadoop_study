fbecb4a1dd558af55b41520377afec097033cb1a
郭帅 <yooless@163.com>
2018-11-29 16:36:18 +0800

    commit

---
+++ Kafka/toyCode/TestKafkaConsumer.java
@@ -0,0 +1,77 @@
+package com.enjoyor.mtdap.test;
+
+import kafka.utils.ShutdownableThread;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * Created with IDEA
+ * author 郭帅
+ * date 15:04 2018/11/29
+ */
+public class TestKafkaConsumer extends ShutdownableThread {
+    private static String bootstrap;
+    private static String groupId;
+    private static String topic;
+    private static String IsAntoCommit;
+    private static String timeOut;
+    private static String maxPollRecords;
+    private static String autoOffsetReset;
+    private static String keySerializer;
+    private static String valueSerializer;
+    private static Properties prop = new Properties();
+    private static KafkaConsumer<String, String> consumer;
+    ConsumerRecords<String, String> msgList;
+
+    TestKafkaConsumer() {
+        /*false 不关闭*/
+        super("TestKafkaConsumer", false);
+    }
+
+    public static void init() {
+        try {
+            prop.load(TestKafkaConsumer.class.getClassLoader().getResourceAsStream("kafka.properties"));
+            bootstrap = prop.getProperty("bootstrap");
+            groupId = prop.getProperty("groupId");
+            topic = prop.getProperty("topic");
+            keySerializer = prop.getProperty("keyDeserializer");
+            valueSerializer = prop.getProperty("valueDeserializer");
+            prop.put("bootstrap.servers", bootstrap);
+            prop.put("group.id", groupId);
+            prop.put("key.deserializer", keySerializer);
+            prop.put("value.deserializer", valueSerializer);
+
+            consumer= new KafkaConsumer<>(prop);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void doWork() {
+        /*初始化*/
+        init();
+        /*订阅topic*/
+        consumer.subscribe(Arrays.asList(topic));
+        while (true) {
+            /*每秒拉多条,放在ConsumerRecords中*/
+            msgList = consumer.poll(1000);
+            for(ConsumerRecord<String,String> record :msgList){
+                /*取出每一条ConsumerRecords中的每一条ConsumerRecord*/
+                System.out.println(record);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TestKafkaConsumer testKafkaConsumer = new TestKafkaConsumer();
+        testKafkaConsumer.doWork();
+    }
+
+
+}
---
+++ Kafka/toyCode/TestProducer.java
@@ -0,0 +1,55 @@
+package com.enjoyor.mtdap.test;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.utils.Utils;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created with IDEA
+ * author 郭帅
+ * date 13:26 2018/11/29
+ */
+public class TestProducer {
+    private static String bootstrap;
+    private static String topicName;
+    private static String keyDeserializer;
+    private static String valueDeserializer;
+    static Properties kafkaProp = new Properties();
+
+    static {
+        try {
+            kafkaProp.load(TestProducer.class.getClassLoader().getResourceAsStream("kafka.properties"));
+            bootstrap = kafkaProp.getProperty("bootstrap");
+            topicName = kafkaProp.getProperty("topic");
+            keyDeserializer = kafkaProp.getProperty("keySerializer");
+            valueDeserializer = kafkaProp.getProperty("valueSerializer");
+            kafkaProp.put("bootstrap.servers", bootstrap);
+            kafkaProp.put("key.serializer", keyDeserializer);
+            kafkaProp.put("value.serializer", valueDeserializer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void run() {
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(kafkaProp);
+        while (true) {
+            String msg = "这就是发送的消息";
+            producer.send(new ProducerRecord<String, String>(topicName,"message", msg));
+            Utils.sleep(3000);
+            /*如果不是一直发送需要 关闭生产者
+            producer.close();*/
+        }
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+}
+
+
